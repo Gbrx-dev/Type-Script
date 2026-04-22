bb507e8f9484991bfe6477d1f9dd1fb869f53530
Gbrx Dev <gabrielhenriquex1566@gmail.com>
2026-04-22 10:26:04 -0300

    type any e void

---
+++ src/A001-type-annotation/A002-tipo-any.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+function showMessage(msg: any) {
+  return msg;
+}
+
+console.log(showMessage([1, 2, 3]));
+console.log(showMessage('Olá'));
+console.log(showMessage(1));
---
+++ src/A001-type-annotation/A002-tipo-void.ts
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+function semRetorno(...args: string[]): void {
+  console.log(args.join(' '));
+}
+
+const pessoa = {
+  nome: 'Gabriel',
+  sobrenome: 'Santos',
+
+  exibirNome(): void {
+    console.log(this.nome + ' ' + this.sobrenome);
+  }
+}
+
+semRetorno('Gabriel', 'Santos');
+pessoa.exibirNome();
+export { pessoa };
